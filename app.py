9579c20ed6496582102e1caff8268f4d42091f75
petrrick <503823696@qq.com>
2023-06-13 11:01:24 -0400

    utils

---
+++ app.py
@@ -0,0 +1,53 @@
+"""
+Shiny app for wetland carbon emissions.
+
+How to use:
+1. Upload your excel file
+    a. The file should have sheet names as the sensore location name.
+    b. The file should be in .xlsx format.
+    c. The date comlumn should be named as "Date".
+
+
+"""
+from shiny import *
+from shiny.types import FileInfo
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import plotly.graph_objects as go
+
+# utils:
+from util import Get_sheet_names, data_prep, get_coordination, create_map
+
+app_ui = ui.page_fluid(
+    ui.panel_title("Wetland carbon emissions"),
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.input_file(
+                "file", "Upload Your excel file here(.xlsx)", accept=[".xlsx"]
+            ),
+        ),
+        ui.panel_main(
+            ui.output_plot("plot", "Correlation Heatmap"),
+        ),
+    ),
+)
+
+
+def sever(input, output, session) -> None:
+    """Server of the app."""
+    df = pd.read_excel(input.file())
+
+    @output
+    @render.plot
+    def plot() -> object:
+        f: FileInfo = input.file()
+        df = pd.read_excel(f[0]["datapath"])
+        poc = sns.heatmap(df.corr(), cbar=True, cmap="coolwarm")
+        return poc
+
+
+app = App(app_ui, sever)
---
+++ util.py
@@ -0,0 +1,106 @@
+"""
+THis script contains utility functions for the project.
+
+Author: Patrick Duan
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+# Missing value Imputation:
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+
+
+# Coodination of sensors:
+def get_coordination(df: pd.DataFrame) -> pd.DataFrame:
+    """This function returns a dataframe of sensor coordination."""
+    coodi = {
+        "DPW": {"lat": 28.0521, "long": -91.0521},
+        "Elm": {"lat": 25.5519, "long": -80.7826},
+        "Esm": {"lat": 25.4379, "long": -80.5946},
+        "Hb1": {"lat": 33.3455, "long": -79.1957},
+        "Ks3": {"lat": 28.7084, "long": -80.7427},
+        "LA1": {"lat": 29.5013, "long": -90.4449},
+        "LA2": {"lat": 29.8587, "long": -90.2869},
+        "NC1": {"lat": 35.8118, "long": -76.7119},
+        "NC2": {"lat": 35.8030, "long": -76.6685},
+        "NC4": {"lat": 35.7879, "long": -75.9038},
+        "xDL": {"lat": 32.5417, "long": -87.8039},
+    }
+
+    coodi_df = pd.DataFrame(coodi).T.reset_index()
+    coodi_df.columns = ["sensor", "lat", "long"]
+
+    return coodi_df
+
+
+def Get_sheet_names(df_path: str) -> list:
+    """Get sheet names from excel file.
+
+    Args:
+        df_path (str): Path to excel file.
+
+    Returns:
+        list: Sheet names.
+    """
+    xl = pd.ExcelFile(df_path)
+    return xl.sheet_names
+
+
+def data_prep(df_path: str, sheet_list: list) -> pd.DataFrame:
+    """Prepare data for analysis.
+
+    Args:
+        df (pd.DataFrame): Raw data.
+
+    Returns:
+        pd.DataFrame: Cleaned data.
+    """
+    # Get sheet names
+    # concatenate all sheets into one dataframe
+    # impute missing values
+    df_list = [
+        pd.read_excel(df_path, sheet_name=sheet).assign(sheet_name=sheet)
+        for sheet in sheet_list
+    ]
+    final_df = pd.concat(df_list)
+
+    imp = IterativeImputer(max_iter=10, random_state=0)
+    new_d = pd.DataFrame(np.round(imp.fit_transform(final_df), 4))
+    new_d.columns = final_df.columns
+    new_d["Date"] = new_d["Date"].astype(int).astype(str)
+    new_d["Date"] = pd.to_datetime(new_d["Date"], format="%Y%m%d")
+
+    return new_d
+
+
+def create_map(df: pd.DataFrame):
+    """
+    This function creates a map of ground sensors.
+    """
+    fig = go.FigureWidget(
+        data=[
+            go.Scattermapbox(
+                lat=df["lat"],
+                lon=df["long"],
+                mode="markers",
+                marker=go.scattermapbox.Marker(
+                    size=14, sizemode="diameter", color="#0d6aff", opacity=0.7
+                ),
+                text=df["sensor"],
+                hoverinfo="text",
+            )
+        ],
+        layout=dict(
+            autosize=True,
+            hovermode="closest",
+            height=600,
+            width=1000,
+            margin=dict(l=0, r=0, b=0, t=0),
+            mapbox=dict(style="open-street-map", zoom=0.5),
+            showlegend=False,
+        ),
+    )
+    return fig
